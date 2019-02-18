db3bd10b449066baf9a9f006fbbcb1b90df2654b
Lev# <mr.lmartirosyan888@gmail.com>
2019-02-18 22:57:13 +0400

    relise one

---
+++ Fibonacchi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double prevFib = 1, curFib = 1;
+
+            for (int i = 0; i <= prevFib; i++)
+            {
+                double newFib = prevFib + curFib;
+                prevFib = curFib;
+                curFib = newFib;
+                Console.WriteLine(prevFib);
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
